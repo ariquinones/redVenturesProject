9dfe4aa90b9c164097d0c70604b711b4f5dc3eb1
Ari Quiñones Vargas <AriQuinones@Aris-MBP.attlocal.net>
2016-05-17 16:00:05 -0500

    mobileFriendly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function showMenu () {
 	if(checkbox.checked) {
-		document.querySelector('.languagesContainer').setAttribute('style', 'visibility: visible; opacity: 1; -o-transform: translate(0,250px); -moz-transform: translate(0,250px); -webkit-transform: translate(0,250px)');
+		document.querySelector('.languagesContainer').setAttribute('style', 'visibility: visible; opacity: 1; top: 3em');
 	}
 	else {
 		document.querySelector('.languagesContainer').setAttribute('style', 'visibility: hidden; opacity: 0');
